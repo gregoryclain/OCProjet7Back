572ff59b1b1d2161e5c8b75e35e3ac7704549d96
Linghz <gc.webmaster69@gmail.com>
2020-06-13 23:46:50 +0200

    get and post proto message

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
+
+// get all messages
 router.get("/message/list", (req, res) => {
   db.Message.findAll().then((messages) => {
     res.send(messages);
+  });
+});
+
+// post message
+router.post("/message/new", (req, res) => {
+  db.Message.create({
+    title: req.body.title,
+    message: req.body.message,
+  }).then((last) => {
+    res.send(last);
   });
 });
 
